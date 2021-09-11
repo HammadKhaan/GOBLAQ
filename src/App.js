897eccb00fce9611711b8d33460795ef0afd64a4
HammadKhaan <hammadkhan.1199@gmail.com>
2021-09-11 05:16:51 +0500

    App Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import 'antd/dist/antd.css';
-import Collections from './Components/Collections';
-import Header from './Components/Header';
-import JoinToday from './Components/JoinToday';
-import Partner from './Components/Partner';
-import Footer from './Components/Footer';
+import "./App.css";
+import "antd/dist/antd.css";
+import Collections from "./Components/Collections";
+import Header from "./Components/Header";
+import JoinToday from "./Components/JoinToday";
+import Partner1 from "./Components/Partner";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Collections/>
-      <Partner/>
-      <JoinToday/>
-      <Footer/>
+      <Header />
+      <Collections />
+      <Partner1 />
+      <JoinToday />
+      <Footer />
     </div>
   );
 }
